34de178e0a392a5fc025c4cb5a0b3d0f4c4c4dca
Jazlan01 <ja.zl.an@live.com>
2022-09-08 23:00:36 +0500

    All testcases pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(rep, times) {
+    
+    if (times == 0) {
+        return ""
+    }
 
+    if (times < 0) {
+        return "ERROR"
+    }
+    
+    let print = rep
+    for (let index = 0; index < times-1; index++) {
+        print = print + rep
+    }
+
+    return print
 };
 
 // Do not edit below this line
